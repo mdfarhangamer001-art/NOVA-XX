09663e33f82404ea8551f38e93ef7d9054a4f6e5
mdfarhangamer001-art <mdfarhangamer001@gmail.com>
2026-07-16 13:11:36 +0530

    Update APP.tsx

--- src/renderer/src/views/APP.tsx
+++ src/renderer/src/views/APP.tsx
@@ -1,4 +1,4 @@
-import { AppItem, getAllApps } from '@renderer/services/system-info'
+import { AppItem, getAllApps } from '@/src/renderer/src/service/'
 import { useState, useEffect, useRef, useCallback } from 'react'
 import {
   RiAppsLine,
