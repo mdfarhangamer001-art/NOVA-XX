615cd411b33fab12ffc29de90205d980b9510416
mdfarhangamer001-art <mdfarhangamer001@gmail.com>
2026-07-16 12:38:00 +0530

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,13 +3,14 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
-import registerAppHandlers from './lib/APP'
-import registerAgentsHandlers from './lib/Agents'
-import registerGalleryHandlers from './lib/Gallery'
-import registerNotesHandlers from './lib/Notes'
-import registerPhoneHandlers from './lib/Phone'
-import registerSettingsHandlers from './lib/Settings'
-import registerGoogleAuthHandlers from './lib/google-auth'
+// यहाँ पाथ अपडेट किए गए हैं ताकि वे renderer/src/views/ को पॉइंट करें
+import registerAppHandlers from '../renderer/src/views/APP'
+import registerAgentsHandlers from '../renderer/src/views/Agents'
+import registerGalleryHandlers from '../renderer/src/views/Gallery'
+import registerNotesHandlers from '../renderer/src/views/Notes'
+import registerPhoneHandlers from '../renderer/src/views/Phone'
+import registerSettingsHandlers from '../renderer/src/views/Settings'
+import registerGoogleAuthHandlers from '../renderer/src/views/google-auth'
 
 let mainWindow: BrowserWindow | null = null
 
@@ -52,6 +53,7 @@ app.whenReady().then(() => {
     optimizer.watchWindowShortcuts(window)
   })
 
+  // हैंडलर्स अब सही पाथ से रजिस्टर होंगे
   registerAppHandlers(ipcMain)
   registerAgentsHandlers(ipcMain)
   registerGalleryHandlers(ipcMain)
