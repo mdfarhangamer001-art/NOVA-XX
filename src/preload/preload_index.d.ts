1870434c742cb64a67007b279ea6893aadc8ed45
mdfarhangamer001-art <mdfarhangamer001@gmail.com>
2026-07-19 07:39:51 +0530

    Add files via upload

---
+++ src/preload/preload_index.d.ts
@@ -0,0 +1,31 @@
+import { ElectronAPI } from '@electron-toolkit/preload'
+
+declare global {
+  interface Window {
+    electron: ElectronAPI & {
+      ipcRenderer: {
+        invoke(channel: string, ...args: any[]): Promise<any>
+        send(channel: string, ...args: any[]): void
+        on(channel: string, func: (...args: any[]) => void): () => void
+        off(channel: string, func: (...args: any[]) => void): void
+      }
+    }
+    api: unknown
+    iris: {
+      sendVisionFrame(base64Frame: string): Promise<any>
+      saveChatHistory(history: any[]): Promise<{ success: boolean; count?: number; error?: string }>
+      loadChatHistory(): Promise<any[]>
+      clearChatHistory(): Promise<{ success: boolean }>
+      transcribeAudio(base64Audio: string, mimeType: string): Promise<any>
+      getMemories(): Promise<any>
+      deleteMemory(index: number): Promise<any>
+      launchApp(appName: string): Promise<any>
+      adbConnect(ip: string, port: string): Promise<any>
+      adbDisconnect(): Promise<any>
+      adbTelemetry(): Promise<any>
+      adbQuickAction(action: string): Promise<any>
+      onTranscript(callback: (data: { role: string; text: string; isFinal: boolean }) => void): void
+      onTranscriptComplete(callback: () => void): void
+    }
+  }
+}
